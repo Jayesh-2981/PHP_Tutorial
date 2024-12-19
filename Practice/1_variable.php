179048c8b15506dbd7a23fad1036d0a292652069
Jayesh <jadhavjayesh949@gmail.com>
2024-12-20 01:05:38 +0530

    php variables

--- Practice/1_variable.php
+++ Practice/1_variable.php
@@ -4,30 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Variables</title>
 </head>
 
 <body>
     <div class="container">
-        <h2>Rules for Creating a varriable</h2>
+        <h2>Rules for Creating a Variable</h2>
         <ul>
             <li>Starts with a $ sign</li>
-            <li>Cannot starts with a number</li>
-            <li>Must starts with a letter or an underscore</li>
-            <li>Can only contain alpha numeric characters and underscores</li>
-            <li>Variables in php are case sensitive. ($Jayesh $JAyesh $JAYESH are different)</li>
+            <li>Cannot start with a number</li>
+            <li>Must start with a letter or an underscore</li>
+            <li>Can only contain alphanumeric characters and underscores</li>
+            <li>Variables in PHP are case sensitive. ($Jayesh, $JAyesh, $JAYESH are different)</li>
         </ul>
     </div>
 
     <?php
-    // echo "This is php variable";
-    // Variables are container for storing the data
-    // Starts with $
-    $name = "Jayesh";
-    $namE = "Capital JAYESH";
-    // Varibales are case sensitive
-    $income = 10000.00;
+    // Defining variables
+    $name = "Jayesh"; // Variable to store the name
+    $namE = "Capital JAYESH"; // Variable with a different case
+    $income = 10000.00; // Variable to store income
 
+    // Outputting the values of the variables
     echo "My name is $name and my income is $income rs <br>";
     echo "$namE is a family man <br>";
     echo "$name is a real gangsta <br><br>";
