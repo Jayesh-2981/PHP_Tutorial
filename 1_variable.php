fba25cf40999e543f11c35e82c4113839da18ced
Jayesh <jadhavjayesh949@gmail.com>
2024-12-19 23:38:47 +0530

    php variables

--- 1_variable.php
+++ 1_variable.php
@@ -1,24 +1,12 @@
-<?php
-// echo "This is php variable";
-// Variables are container for storing the data
-// Starts with $
-$name = "Jayesh";
-$namE = "Capital JAYESH";
-// Varibales are case sensitive
-$income = 10000.00;
-
-echo "My name is $name and my income is $income rs <br>";
-echo "$namE is a family man <br>";
-echo "$name is a real gangsta <br><br>";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <h2>Rules for Creating a varriable</h2>
@@ -30,5 +18,21 @@ echo "$name is a real gangsta <br><br>";
             <li>Variables in php are case sensitive. ($Jayesh $JAyesh $JAYESH are different)</li>
         </ul>
     </div>
+
+    <?php
+    // echo "This is php variable";
+    // Variables are container for storing the data
+    // Starts with $
+    $name = "Jayesh";
+    $namE = "Capital JAYESH";
+    // Varibales are case sensitive
+    $income = 10000.00;
+
+    echo "My name is $name and my income is $income rs <br>";
+    echo "$namE is a family man <br>";
+    echo "$name is a real gangsta <br><br>";
+    ?>
+
 </body>
+
 </html>
