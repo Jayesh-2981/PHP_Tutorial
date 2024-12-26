628a0a28512bcb271ac5f15593ffba047754ad8e
Jayesh <jadhavjayesh949@gmail.com>
2024-12-26 18:12:01 +0530

    for loop

--- Practice/8_for_loops.php
+++ Practice/8_for_loops.php
@@ -11,19 +11,38 @@
     <?php
     echo "This is for loop in action <br>";
 
-    for ($index = 0; $index <= 5; $index++) {
-        // for (initialization; condition; updation)
+    // Define the loop parameters
+    $start = 0;
+    $end = 5;
+
+    // Use a for loop to iterate over the range
+    for ($index = $start; $index <= $end; $index++) {
         echo "The index is $index <br>";
+    }
+
+    echo "For loop has ended <br>";
+
+    // Example of a nested for loop
+    echo "Nested for loop example: <br>";
+    for ($i = 1; $i <= 3; $i++) {
+        for ($j = 1; $j <= 3; $j++) {
+            echo "($i, $j) <br>";
+        }
+    }
+
+    // Example of a for loop with an array
+    $colors = array("Red", "Green", "Blue");
+    echo "For loop with an array example: <br>";
+    for ($i = 0; $i < count($colors); $i++) {
+        echo "$colors[$i] <br>";
     }
 
     // Avoid running into infinite loops
     // for ($i=0; $i < 87; ) {
     // echo $i;
     // }
-
-    echo "For loop has ended <br>";
-
     ?>
+
 </body>
 
 </html>
