60e6610c5e1961ebaea7f29a30d353d705f03d97
Jayesh <jadhavjayesh949@gmail.com>
2025-01-01 21:23:56 +0530

    creating databse in mysql using php

---
+++ Practice/18_MySql_DB.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creating MySql database</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1><center>Creating MySql Database<br></center></h1>";
+
+    // Define database connection parameters
+    $servername = "localhost";
+    $username = "root";
+    $password = ""; // Make it empty otherwise connection will fail and we got an error
+
+    // Create a connection
+    $conn = mysqli_connect($servername, $username, $password);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        echo "Connected successfully";
+    }
+
+    // Close the connection when you're done with it
+    mysqli_close($conn);
+    ?>
+
+</body>
+
+</html>
